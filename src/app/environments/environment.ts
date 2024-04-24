818f2fee4b205e5195d0fe04eb1f4292d71a6e53
Premodial <45264179+Premodial@users.noreply.github.com>
2024-04-24 23:04:44 +0200

    Update environment.ts

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,11 +1,5 @@
 export const mapboxAccessToken = "pk.eyJ1IjoicHJlbW9kaWFsIiwiYSI6ImNsdW8zaDB5MjAwbmMybW54cWZxOTJuN3kifQ.2qaG1CXHVOfNxtZ_jDiI9Q";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-    apiKey: "AIzaSyCHLo1Q9_PjqP-_3v75xGbLLGltzhwFvOg",
-    authDomain: "weather-app-f801d.firebaseapp.com",
-    projectId: "weather-app-f801d",
-    storageBucket: "weather-app-f801d.appspot.com",
-    messagingSenderId: "744325746100",
-    appId: "1:744325746100:web:db6968eb311718e119b6e1",
-    measurementId: "G-TN02EX5JSP"
+
 };
